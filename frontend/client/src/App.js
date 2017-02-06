70bbb87a3146fb70a710ce607465726d41d9d51c
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2017-02-06 19:44:59 +0100

    initial commit

---
+++ frontend/client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="page-header">
+          <h1>The Traveling Salesman&#039;s Planner</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
