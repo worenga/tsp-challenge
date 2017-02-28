dcf03bc2a82307492be7a1f2381a7768a46ef152
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2017-02-28 13:31:48 +0100

    cleanup

--- frontend/client/src/constants/config.js
+++ frontend/client/src/constants/config.js
@@ -1,3 +1,3 @@
-export const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY || "AIzaSyDKEUyP74Ul4tzVaFaxypn86VFjN-zU7Bw"
-export const MAX_PLACES = 10
+export const GOOGLE_MAPS_API_KEY = process.env.TSP_CHALLENGE_GOOGLE_MAPS_API_KEY || "AIzaSyDKEUyP74Ul4tzVaFaxypn86VFjN-zU7Bw"
+export const MAX_PLACES = process.env.TSP_CHALLENGE_MAX_PLACES || 10;
 export default {}
